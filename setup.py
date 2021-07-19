2ace8f01464cabd2bb1837d757b91c8934fcb830
kapitalisman <sam.oosterveer@gmail.com>
2021-07-19 16:53:22 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+with open('README.md', 'r') as readme_file:
+    readme = readme_file.read()
+
+requirements = ['requests>=2']
+
+setup(
+    name='xignitegh',
+    version='0.0.1',
+    author='Kapitalisman',
+    author_email='hallo@kapitalisman.nl',
+    license='MIT',
+    description='A wrapper to the XigniteGlobalHistorical API',
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    url='https://github.com/xignite-python/xignitegh',
+    python_requires='>=3',
+    packages=find_packages(),
+    install_requires=requirements,
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.7',
+    ],
+    keywords='xignite api wrapper',
+)
